cc3a63cc7941b63f62349de2f580cf84f3185b17
RamiroCyber <ramiro.dr@gmail.com>
2021-05-05 13:54:38 -0300

    SecurityConfig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,13 +14,13 @@ function App() {
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
             <BarChart />
-          </div>
+          
         </div>
         <div className="col-sm-6">
           <h5 className="text-center text-secondary">Todas as Vendas</h5>
           <DonutChart />
         </div>
-
+        </div>
         <div className="py-3">
           <h2 className="text-primary">Todas as Vendas</h2>
 
